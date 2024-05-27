2f6b17ae079146742ef1d21488a5cae8ca9e5a9d
jianliang <jianliangw1@gmail.com>
2024-05-27 23:38:44 +0800

    fix: show chat box in popup

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from '@assets/img/logo.png';
 import '@pages/popup/Popup.css';
 import useStorage from '@src/shared/hooks/useStorage';
 import exampleThemeStorage from '@src/shared/storages/exampleThemeStorage';
 import withSuspense from '@src/shared/hoc/withSuspense';
 import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
-import Settings from '@pages/content/Settings/Settings';
+import ChatBox from '@pages/components/ChatBox';
+import { getSystemPrompt } from '@src/agent/agent';
+import AgentsLoader from '@src/agent/agentsRegister';
+import { Divider, Spinner } from '@chakra-ui/react';
+import { createNewSession } from '@pages/content/storageUtils';
 
 const Popup = () => {
   const theme = useStorage(exampleThemeStorage);
+  const [agent, setAgent] = useState(null);
+  const [sessionId, setSessionId] = useState(-1);
+
+  useEffect(() => {
+    const agentsRegistry = new AgentsLoader();
+    agentsRegistry.loadAgents().then(agents => {
+      const agent = agents.get('chat-with-the-bot');
+      setAgent(agent);
+      createNewSession('Chat with the bot', agent).then(sessionId => {
+        setSessionId(sessionId);
+      });
+    });
+  }, []);
 
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor: theme === 'light' ? '#fff' : '#000',
-      }}>
+    <div className="Popup-App">
       <header className="App-header" style={{ color: theme === 'light' ? '#fff' : '#000' }}>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <Settings />
+      <Divider mb={2} />
+      {agent && sessionId > 0 ? (
+        <ChatBox
+          preInput=""
+          inputType="default"
+          systemPrompt={getSystemPrompt(agent, 'default')}
+          sessionId={sessionId}
+          newMessages={[]}
+          minW="400px"
+          maxH="600px"
+        />
+      ) : (
+        <Spinner />
+      )}
     </div>
   );
 };
